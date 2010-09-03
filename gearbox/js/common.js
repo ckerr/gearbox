085b80f95d7d137a87a699caf86078eb9604de5c
Charles Kerr <charles@transmissionbt.com>
2010-09-03 14:13:47 -0500

    remove duplicate code

--- gearbox/js/common.js
+++ gearbox/js/common.js
@@ -57,26 +57,6 @@ Number.prototype.toTruncFixed = function( place ) {
         return ret.toFixed( place );
 }
 
-
-function parseUri( sourceUri )
-{
-    var uriPartNames = ["source","protocol","authority","domain","port","path","directoryPath","fileName","query","anchor"];
-    var uriParts = new RegExp("^(?:([^:/?#.]+):)?(?://)?(([^:/?#]*)(?::(\\d*))?)?((/(?:[^?#](?![^?#/]*\\.[^?#/.]+(?:[\\?#]|$)))*/?)?([^?#/]*))?(?:\\?([^#]*))?(?:#(.*))?").exec(sourceUri);
-    var uri = {};
-    
-    for(var i = 0; i < 10; i++){
-        uri[uriPartNames[i]] = (uriParts[i] ? uriParts[i] : "");
-    }
-    
-    // Always end directoryPath with a trailing backslash if a path was present in the source URI
-    // Note that a trailing backslash is NOT automatically inserted within or appended to the "path" key
-    if(uri.directoryPath.length > 0){
-        uri.directoryPath = uri.directoryPath.replace(/\/?$/, "/");
-    }
-    
-    return uri;
-}
-
 function getHost( sourceUri )
 {
     var domain = parseUri(sourceUri).domain;
