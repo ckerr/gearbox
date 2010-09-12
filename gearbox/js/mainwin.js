e545e20847859abe804c84d94f0e46a6818c49f1
Longinus00 <Longinus00@gmail.com>
2010-09-12 10:08:35 -0700

    fix torrent selections when changing sort/filter

--- gearbox/js/mainwin.js
+++ gearbox/js/mainwin.js
@@ -456,14 +456,17 @@ Ext.namespace( 'Transmission' );
     }
     function refilter( )
     {
+        var selected = torrentView.getSelectedRecords();
         Torrent.store.filterBy( filterFunc, this );
         updateTorrentCount( );
+        reSelect( selected );
     }
 
     function resort( )
     {
         var fieldName;
         var desc = myPrefs.getBool('sort-reversed');
+        var selected = torrentView.getSelectedRecords();
 
         switch( myPrefs.get('sort-mode') ) {
             case 'activity':  fieldName = 'rateXfer';     desc=!desc; break;
@@ -479,6 +482,23 @@ Ext.namespace( 'Transmission' );
         var dir = desc ? 'DESC' : 'ASC';
         Torrent.store.sort( fieldName, dir );
         Torrent.store.setDefaultSort( fieldName, dir );
+        reSelect( selected );
+    }
+
+    function reSelect( records )
+    {
+        if( records.length < 1 )
+            return;
+
+        var newRecords = torrentView.getRecords( torrentView.getNodes() );
+        var ids = [];
+        var s = [];
+
+        for( var i=0, n=records.length; i<n; ++i )
+            if( newRecords.indexOf( records[i] ) != -1 )
+                s.push( records[i] );
+
+        torrentView.select( s );
     }
 
     function onPrefsChanged( keys )
