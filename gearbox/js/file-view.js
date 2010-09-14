ckerr/gearbox

commit ad7b6d02972673c5388df7f29da2632e2bf68705

use Nicholas Zakas' suggestions for speeding up js loops http://www.youtube.com/watch?v=mHtdZgou0qU

gearbox/js/file-view.js
@@ -46,9 +46,10 @@ FileView = Ext.extend( Ext.Container,
             return record.data.wanted ? this.Yes : this.No;
 
         var wanted;
-        for( var i=0, n=record.data.children.length; i<n; ++i )
+        var children = record.data.children;
+        for( var i=children.length; i--; )
         {
-            var child = this.store.getById( record.data.children[i] );
+            var child = this.store.getById( children[i] );
             var childWanted = this.isSubtreeWanted( child );
 
             if( i === 0 )
@@ -70,7 +71,7 @@ FileView = Ext.extend( Ext.Container,
         else
         {
             var children = record.data.children;
-            for( var i=0, n=children.length; i<n; ++i )
+            for( var i=children.length; i--; )
                 this.getDescendantLeaves( this.store.getById( children[i] ), fileIndices );
         }
     },
@@ -102,9 +103,10 @@ FileView = Ext.extend( Ext.Container,
             return record.data.priority;
 
         var priority;
-        for( var i=0, n=record.data.children.length; i<n; ++i )
+        var children = record.data.children;
+        for( var i=children.length; i--; )
         {
-            var child = this.store.getById( record.data.children[i] );
+            var child = this.store.getById( children[i] );
             var childPriority = this.getPriority( child );
 
             if( i === 0 )
@@ -211,16 +213,18 @@ FileView = Ext.extend( Ext.Container,
     getSize: function( record )
     {
         var ret = { size:0, bytesCompleted:0 };
-        if( record.data._is_leaf )
+        var d = record.data;
+        if( d._is_leaf )
         {
-            ret.size = record.data.size;
-            ret.bytesCompleted = record.data.bytesCompleted;
+            ret.size = d.size;
+            ret.bytesCompleted = d.bytesCompleted;
         }
         else
         {
-            for( var i=0, n=record.data.children.length; i<n; ++i )
+            var children = d.children;
+            for( var i=children.length; i--; )
             {
-                var child = this.store.getById( record.data.children[i] );
+                var child = this.store.getById( children[i] );
                 var s = this.getSize( child );
                 ret.size += s.size;
                 ret.bytesCompleted += s.bytesCompleted;
@@ -353,15 +357,16 @@ FileView = Ext.extend( Ext.Container,
     {
         var stats = torrent.getFileStats();
 
-        for( var i=0, n=stats.length; i<n; ++i )
+        for( var i=stats.length; i--; )
         {
             var record = this.store.getById( i );
             if( record != null )
             {
+                var s = stats[i];
                 record.beginEdit();
-                record.set( 'wanted', stats[i].wanted );
-                record.set( 'priority', stats[i].priority );
-                record.set( 'bytesCompleted', stats[i].bytesCompleted );
+                record.set( 'wanted', s.wanted );
+                record.set( 'priority', s.priority );
+                record.set( 'bytesCompleted', s.bytesCompleted );
                 record.endEdit();
             }
         }
@@ -373,7 +378,7 @@ FileView = Ext.extend( Ext.Container,
     {
         var records = this.store.getRange( );
 
-        for( var i=0, n=records.length; i<n; ++i )
+        for( var i=records.length; i--; )
         {
             var record = records[i];
             if( record.data._is_leaf )

gearbox/js/app.js
@@ -48,7 +48,7 @@ Ext.onReady(function()
 
         // updated torrents...
         var torrents = args.torrents;
-        for( var i=0, n=torrents.length; i<n; ++i ) {
+        for( var i=torrents.length; i--; ) {
             var torrent = torrents[i];
             var r = store.getById( torrent.id );
             if( r ) {
@@ -70,7 +70,7 @@ Ext.onReady(function()
         // removed torrents...
         var removed = args.removed;
         if( removed )
-            for( var i=0, n=removed.length; i<n; ++i )
+            for( var i=removed.length; i--; )
                 store.remove( store.getById( removed[i] ) );
 
         config.mainwin.setTorrentStore( store );

gearbox/js/mainwin.js
@@ -59,7 +59,7 @@ Ext.namespace( 'Transmission' );
     {
         var n = records.length;
         var ids = new Array( n );
-        for( var i=0; i<n; ++i )
+        for( var i=n; i--; )
             ids[i] = records[i].getId( );
         return ids;
     }
@@ -85,7 +85,7 @@ Ext.namespace( 'Transmission' );
             return;
 
         var ids = new Array( n );
-        for( var i=0; i<n; ++i )
+        for( var i=n; i--; )
         {
             var tor = records[i];
 
@@ -293,13 +293,13 @@ Ext.namespace( 'Transmission' );
         var records = torrentView.getSelectedRecords();
         var selectedCount = records.length;
         var selectedPausedCount = 0;
-        for( var i=0; i<selectedCount; ++i )
+        for( var i=selectedCount; i--; )
             if( records[i].isPaused( ) )
                 ++selectedPausedCount;
 
         var allCount = records.length;
         var pausedCount = 0;
-        for( var i=0; i<allCount; ++i )
+        for( var i=allCount; i--; )
             if( records[i].isPaused( ) )
                 ++pausedCount;
 
@@ -376,17 +376,15 @@ Ext.namespace( 'Transmission' );
     {
         var hash = { };
         var allrecs = getAllRecordsUnfiltered();
-        for( var i=0; i<allrecs.length; ++i ) {
+        for( var i=allrecs.length; i--; ) {
             var trackers = allrecs[i].data.trackers;
-            for( var j=0; j<trackers.length; ++j ) {
+            for( var j=trackers.length; j--; ) {
                 var tracker = trackers[j];
                 hash[tracker.readableHost] = tracker.host;
             }
         }
 
-        var keys = [ ];
-        for( var key in hash )
-            keys.push( key );
+        var keys = Object.keys(hash);
         keys.sort( );
 
         // if the list of trackers has changed update the "trackers" button's menu
@@ -439,8 +437,9 @@ Ext.namespace( 'Transmission' );
         if( tracker === 'all' )
             return true;
 
-        for( var i=0, n=rec.data.trackers.length; i<n; ++i )
-            if( rec.data.trackers[i].announce.indexOf( tracker ) >= 0 )
+        var trackers = rec.data.trackers;
+        for(var i=trackers.length; i--; )
+            if( trackers[i].announce.indexOf( tracker ) >= 0 )
                 return true;
 
         return false;
@@ -489,7 +488,7 @@ Ext.namespace( 'Transmission' );
         var doLayout = false;
         var doTurtleTooltip = false;
 
-        for( var i=0, n=keys.length; i<n; ++i )
+        for( var i=keys.length; i--; )
         {
             var key = keys[i];
 

gearbox/js/peer-view.js
@@ -37,7 +37,7 @@ PeerView = Ext.extend( Ext.grid.GridPanel,
         var peerStore = this.store;
         var peers = record.data.peers;
         var newPeers = [ ];
-        for( var i=0, n=peers.length; i<n; ++i )
+        for(var i=peers.length; i--; )
         {
             var peer = peers[i];
 
@@ -60,7 +60,7 @@ PeerView = Ext.extend( Ext.grid.GridPanel,
 
         // removed peers...
         var oldPeers = peerStore.getRange( );
-        for( var i=0, n=oldPeers.length; i<n; ++i )
+        for( var i=oldPeers.length; i--; )
             if( newPeers.indexOf( oldPeers[i].data.address ) == -1 )
                 peerStore.remove( oldPeers[i] );
     },

gearbox/js/prefs-dialog.js
@@ -242,7 +242,7 @@ Ext.namespace( 'Transmission' );
 
     function onPrefsChanged( keys )
     {
-        for( var i=0, n=keys.length; i<n; ++i )
+        for(var i=keys.length; i--; )
         {
             var key = keys[i];
             var e = Ext.getCmp( key );

gearbox/js/torrent.js
@@ -97,7 +97,7 @@ Torrent.Record.prototype.set = function(key,val){
                 break;
             case 'trackers': {
                 var trackers = this.data.trackers;
-                for(var i=0; i<trackers.length; ++i) {
+                for(var i=trackers.length; i--; ) {
                     var t = trackers[i];
                     t.uri = parseUri(t.announce);
                     t.host = getHost(t.uri);
@@ -129,13 +129,13 @@ Torrent.store.getUnfilteredCount = function(){
 
 // create the derived fields when necessary
 Torrent.store.addListener( 'add', function( store, records, index ) {
-    for( var i=0, n=records.length; i<n; ++i ) {
+    for(var i=records.length; i--; ) {
         var d = records[i].data;
         d.collatedName = Ext.util.Format.lowercase(d.name.trim());
         d.rateXfer = (d.rateUpload||0) + (d.rateDownload||0);
         d.state = d.error ? -1 : d.status;
         var trackers = d.trackers;
-        for(var j=0; j<trackers.length; ++j) {
+        for(var j=trackers.length; j--; ) {
             var t = trackers[j];
             t.uri = parseUri(t.announce);
             t.host = getHost(t.uri);

gearbox/js/tracker-view.js
@@ -187,7 +187,7 @@ TrackerView = Ext.extend( Ext.Container,
 
     onPrefsChanged: function( keys )
     {
-        for( var i=0, n=keys.length; i<n; ++i )
+        for( var i=keys.length; i--; )
         {
             var key = keys[i];
 
@@ -277,15 +277,16 @@ TrackerView = Ext.extend( Ext.Container,
     onRemoveClicked: function( )
     {
         var records = this.gridPanel.getSelectionModel().getSelections();
-        var urls = new Array( records.length );
-        for( var i=0, n=records.length; i<n; ++i )
+        var n = records.length;
+        var urls = new Array( n );
+        for( var i=n; i--; )
             urls[i] = records[i].data.announce;
         this.session.removeTrackers( this.torrentId, urls );
     },
 
     onRecordsLoaded: function( store, records, options )
     {
-        for( var i=0, n=records.length; i<n; ++i )
+        for( var i=records.length; i--; )
         {
             var data = records[i].data;
             data.uri = data.uri || parseUri(data.announce);

commit 45d9bca9c0397873959e671994c840d917593376

fix file-view bug I introduced in ad7b6d02972673c5388d

gearbox/js/file-view.js
@@ -47,7 +47,7 @@ FileView = Ext.extend( Ext.Container,
 
         var wanted;
         var children = record.data.children;
-        for( var i=children.length; i--; )
+        for( var i=0, n=children.length; i<n; i++ )
         {
             var child = this.store.getById( children[i] );
             var childWanted = this.isSubtreeWanted( child );
@@ -71,7 +71,7 @@ FileView = Ext.extend( Ext.Container,
         else
         {
             var children = record.data.children;
-            for( var i=children.length; i--; )
+            for( var i=0, n=children.length; i<n; ++i )
                 this.getDescendantLeaves( this.store.getById( children[i] ), fileIndices );
         }
     },
@@ -104,7 +104,7 @@ FileView = Ext.extend( Ext.Container,
 
         var priority;
         var children = record.data.children;
-        for( var i=children.length; i--; )
+        for( var i=0, n=children.length; i<n; ++i )
         {
             var child = this.store.getById( children[i] );
             var childPriority = this.getPriority( child );
@@ -219,7 +219,7 @@ FileView = Ext.extend( Ext.Container,
         else
         {
             var children = d.children;
-            for( var i=children.length; i--; )
+            for( var i=0, n=children.length; i<n; ++i )
             {
                 var child = this.store.getById( children[i] );
                 var s = this.getSize( child );
@@ -353,7 +353,7 @@ FileView = Ext.extend( Ext.Container,
     {
         var stats = torrent.getFileStats();
 
-        for( var i=stats.length; i--; )
+        for( var i=0, n=stats.length; i<n; ++i )
         {
             var record = this.store.getById( i );
             if( record )
@@ -374,7 +374,7 @@ FileView = Ext.extend( Ext.Container,
     {
         var records = this.store.getRange( );
 
-        for( var i=records.length; i--; )
+        for( var i=0, n=records.length; i<n; ++i )
         {
             var record = records[i];
             if( record.data._is_leaf )